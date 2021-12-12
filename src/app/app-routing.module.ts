8daf98a095bf835ed6c81050439e357c73b4adef
Thiago da Silva Adriano <tadriano.net@gmail.com>
2021-12-12 19:07:03 -0300

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('../app/pages/home/home-routing.module').then(m => m.HomeRoutingModule)
+  },
+  {
+    path: '', redirectTo: '/home', pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
